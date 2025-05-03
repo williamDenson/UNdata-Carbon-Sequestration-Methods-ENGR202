a64c2b075269df105cb4efeac92f081d670ea741
William Denson <william.acson@gmail.com>
2025-05-02 20:21:39 -0400

    Add files via upload

---
+++ Carbon_Sequestration_Project_ENGR202.m
@@ -0,0 +1,7 @@
+% ENGR202 Project - Carbon Sequestration Methods
+% NAE Challenge
+% Members:
+% Valerie Swafford
+% William Denson
+% The aim of this MATLAB script is to document and graph data collected by
+% UNdata regarding atmospheric contaminants and how they may be reduced.
