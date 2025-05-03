f49affb502020aeeeadc635d46e71203e3dbfdf5
William Denson <willde4@mail.regent.edu>
2025-05-03 12:09:51 -0400

    Update Carbon_Sequestration_Project_ENGR202.m
    Code changes to reflect the following UNdata set: "Carbon dioxide (CO2) Emissions without Land Use, Land-Use Change and Forestry (LULUCF), in kilotonne CO2 equivalent"
    
    https://data.un.org/Data.aspx?d=GHG&f=seriesID%3aCO2

--- Carbon_Sequestration_Project_ENGR202.m
+++ Carbon_Sequestration_Project_ENGR202.m
@@ -6,5 +6,13 @@
 % The aim of this MATLAB script is to document and graph data collected by
 % UNdata regarding atmospheric contaminants and how they may be reduced.
 
+clc
+clear all
 
-% xlsread to extract data
+Year = xlsread('UNdata_Export_20250503_165309045.xlsx','B2:B1990');
+
+CarbonLevel = xlsread('UNdata_Export_20250503_165309045.xlsx','C2:C1990');
+
+figure(1)
+plot(Year,CarbonLevel,'ro'); hold on;
+
